53a654e1f86d8d050010243654f9c1d2640c75d3
John Siracusa <siracusa@gmail.com>
2024-12-14 07:17:00 -0500

    Initial Commit

---
+++ SwiftUIUntruncatedText/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIUntruncatedText
+//
+//  Created by John Siracusa on 12/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIUntruncatedText/SwiftUIUntruncatedTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIUntruncatedTextApp.swift
+//  SwiftUIUntruncatedText
+//
+//  Created by John Siracusa on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIUntruncatedTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
